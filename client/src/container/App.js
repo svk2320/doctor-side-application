0ebc780f14ccbad666851b0a0fd540b694684a5f
vasanth2320 <vasanthkumars2025@gmail.com>
2023-05-25 20:06:12 +0530

    added lines for app.js

--- client/src/container/App.js
+++ client/src/container/App.js
@@ -14,12 +14,11 @@ const App = () => {
     <Container maxWidth="xl">
       {/* <Navbar /> */}
       <Switch>
-        {/* <Route path="/" exact component={() => <Redirect to="/posts" />} /> */}
+        <Route path="/" exact component={() => <Redirect to="/posts" />} />
         <Route path="/posts" exact component={Home} />
         <Route path="/posts/search" exact component={Home} />
         <Route path="/posts/:post_id" exact component={PostDetails} />
-        {/* <Route path="/auth" exact component={(user) => (user ? <Auth /> : <Redirect to="/posts" />)} /> */}
-        <Route path="/" exact component={<Auth />} />
+        <Route path="/auth" exact component={(user) => (user ? <Auth /> : <Redirect to="/posts" />)} />
       </Switch>
     </Container>
   );
